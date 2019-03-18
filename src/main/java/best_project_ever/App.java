db61b213593c1f792403a0a3682fc58f15f8e2b8
Madalina Mircea <mirceamariamadalina@yahoo.com>
2019-03-18 13:22:24 +0200

    Initial commit

---
+++ src/main/java/best_project_ever/App.java
@@ -0,0 +1,13 @@
+package best_project_ever;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/best_project_ever/AppTest.java
@@ -0,0 +1,20 @@
+package best_project_ever;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
